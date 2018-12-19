79c6ce66caef23f6a33612e8d08e2513f5dcab6c
BilalyC <bilaly.camara@outlook.com>
2018-12-19 13:06:38 +0100

    Exercice 3 Complet

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veillez taper votre âge :");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veillez indiquer votre sexe (homme ou femme) :");
+            string sexe = Console.ReadLine();
+
+            if (age >= 18)
+            {
+                if (sexe == "homme")
+                {
+                    Console.WriteLine("Vous avez " + age + " ans, vous êtes donc majeur.");
+                }
+                else
+                {
+                    if (sexe == "femme")
+                        Console.WriteLine("Vous avez " + age + " ans, vous êtes donc majeure.");
+                    else
+                        Console.WriteLine("Il y a une erreur dans la saisie.");
+                }
+            }
+            else
+            {
+                if (sexe == "homme")
+                {
+                    Console.WriteLine("Vous avez " + age + " ans, vous êtes donc mineur.");
+                }
+                else
+                {
+                    if (sexe == "femme")
+                        Console.WriteLine("Vous avez " + age + " ans, vous êtes donc mineure.");
+                    else
+                        Console.WriteLine("Il y a une erreur dans la saisie.");
+                }
+            }
+                
+        }
+    }
+}
